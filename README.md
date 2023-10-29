b51de5b1248cffb3390b2e123bf50007dece33b1
cdutten-s1 <cesar.dutten@sentinelone.com>
2023-10-29 02:33:43 +0200

    first code commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # cars-analyzer
+
+## Description
+
+The idea of this project is to analyze the data of cars and their prices in the market. 
+
+I will grab the data from https://www.autoscout24.com/, which a website for buying and selling cars in Europe.
+
+The data will be exposed for prometheus to scrape it and then I will use Grafana to visualize the data.
+
+Also, will create a ML model to predict the price of the cars, based on:
+* Make and model
+* Year
+* Mileage
+* Fuel type
+* Transmission
+
+## Features
+* Keep track of the cars that are being sold in the market
+* Keep track of the prices of the cars
+* Keep track of the features of the cars
+* Keep track of the location of the cars
+* Keep track of the time of the cars being sold
---
+++ car-analyzer.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from prometheus_client import start_http_server, Gauge
+
+
+# Create a function that takes a url and returns a dictionary with the data we have collected
+def get_car_data(url):
+    raw_html = requests.get(url)
+    soup = BeautifulSoup(raw_html.content, 'html.parser')
+
+    price_html = soup.find_all(name='span', attrs={'class': re.compile('PriceInfo_price__.*')})
+    price = price_html[0].text
+    price = price[2:-2]
+    price = float(price.replace(',', ''))
+
+    mileage_html = soup.find(name='dt', string='Mileage').find_next_sibling('dd').text
+    mileage_html = mileage_html.replace('km', '')
+    mileage_html = mileage_html.replace(',', '')
+    mileage_html = int(mileage_html)
+
+    first_registration_html = soup.find(name='div', string='First registration') \
+        .find_next_sibling('div') \
+        .find_next_sibling('div').text
+    make_model_html = soup.find(name='div', attrs={'class': re.compile('StageTitle_makeModelContainer__.*')})
+    make = make_model_html.find_all('span')[0].text
+    model = make_model_html.find_all('span')[1].text
+
+    # Find with soup the A tag with the class LocationWithPin_locationItem__.* and get the text
+    location_html = soup.find(name='a', attrs={'class': re.compile('LocationWithPin_locationItem__.*')})
+    location = location_html.text
+
+    return {
+        'price': price,
+        'mileage': mileage_html,
+        'first_registration': first_registration_html,
+        'make': make,
+        'model': model,
+        'location': location,
+        'url': url
+    }
+
+
+# Expose the car data with the prometheus client
+def expose_car_data(car_data):
+    price_gauge = Gauge('car_price', 'Price of the car', ['make', 'model', 'location', 'url'])
+    mileage_gauge = Gauge('car_mileage', 'Mileage of the car', ['make', 'model', 'location', 'url'])
+    first_registration_gauge = Gauge('car_first_registration', 'First registration of the car',
+                                     ['make', 'model', 'location', 'url'])
+    price_gauge.labels(car_data['make'], car_data['model'], car_data['location'], car_data['url']).set(
+        car_data['price'])
+    mileage_gauge.labels(car_data['make'], car_data['model'], car_data['location'], car_data['url']).set(
+        car_data['mileage'])
+    first_registration_gauge.labels(car_data['make'], car_data['model'], car_data['location'], car_data['url']).set(
+        car_data['first_registration'])
+
+
+cars_to_analyze = [
+    'https://www.autoscout24.com/offers/audi-a5-coupe-3-2-fsi-quattro-s-line-plus-xenon-b-o-gasoline-black-e6193db6-e5a4-4ef3-a179-8a73a93fec2b?ipc=recommendation&ipl=homepage-bestresult-listings&position=1&source_otp=t10&source=homepage_last-search',
+    'https://www.autoscout24.com/offers/mercedes-benz-cls-350-7g-tronic-gasoline-silver-c1b46f5f-d7a6-4a63-88ec-cb5baf02413c?ipc=recommendation&ipl=homepage-engine-itemBased&position=3&source_otp=nfm&source=homepage_recommender'
+]
+for car in cars_to_analyze:
+    get_car_data(car)
+
+# start http server for prometheus
+start_http_server(8000)
